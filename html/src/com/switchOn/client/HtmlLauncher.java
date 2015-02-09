0de216103ad6efe31f721c958e43e1afff8e5fd7
Roubaty Anthony <anthony.roubaty@gmail.com>
2015-02-09 15:22:01 +0100

    Initial Project

---
+++ html/src/com/switchOn/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.switchOn.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.switchOn.switchOn;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new switchOn();
+        }
+}
