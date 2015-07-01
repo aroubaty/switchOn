62f91c1ea6206c80104c18cf6f4fc951178dc377
Roubaty Anthony <anthony.roubaty@gmail.com>
2015-07-01 14:57:03 +0200

    design 1.0

--- core/src/com/switchOn/finals/Finals.java
+++ core/src/com/switchOn/finals/Finals.java
@@ -1,8 +1,8 @@
 package com.switchOn.finals;
 
 public class Finals {
-	public static final int SCREEN_WIDTH = 1280;
-	public static final int SCREEN_HEIGHT = 720;
+	public static final int SCREEN_WIDTH = 960;
+	public static final int SCREEN_HEIGHT = 640;
 	
 	//levelMap
 	public static final int MAP_PER_WORLD = 9;
